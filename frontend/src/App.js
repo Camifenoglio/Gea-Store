14902e060de680ab92f51464405d02c054ad807a
Camifenoglio <cami.fenoglio@gmail.com>
2022-07-17 15:00:11 -0300

    new proyect

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ultimo esfuerzo!</h1>
+      <Main></Main>
+    </div>
+  );
+}
+
+export default App;
