c6ae78501910aa13b9c8232b17680479bae40519
robermau <robertom.perez@outlook.com>
2022-07-18 12:25:46 -0300

    users v1

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    fullName: { type: String, required: true },
+    email:{ type: String, required: true },
+    password:[{ type: String, required: true }],
+    imageUser: { type: String, required: true },
+    favorite:{ type: Array },
+    buying:{ type: Array, required: true },
+    rol:{type:Array},
+    uniqueString:{type:String, required:true},
+    verification:{type:Boolean , required:true}
+
+})
+
+
+const User = mongoose.model('users', userSchema)
+module.exports = User
