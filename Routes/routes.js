8ed84352bf0307708719ecc347baf5f7ae01cd4a
Marco Castellacci <marco.castellacci87@gmail.com>
2022-07-19 13:14:27 -0300

    Router Create

--- Routes/routes.js
+++ Routes/routes.js
@@ -15,19 +15,11 @@ Router.route('/products')
 .get(getAllProducts)
 .post(createProduct)
 
-<<<<<<< HEAD
 Router.route('/products/:id')
 .get(getProduct)
 .put(modifyProduct)
 .delete(deleteProduct)
 .post(addFavorite)
-=======
-const usersControllers = require("../controlers/usersControllers")
-const {signUpUsers , logInUser,verifyMail,verifyToken} = usersControllers;
-
-
-
->>>>>>> 78a9d4e0d2af5c4e5e214745f88263896ac9e6d9
 
 Router.route('/all/products')
 .post(createMultlipeProduct)
