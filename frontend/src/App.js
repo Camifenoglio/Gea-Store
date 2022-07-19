94b76efba35ca4a63486460e39381d5818ecb4f7
Bruno Ereza Schork <Brunoereza2014@gmail.com>
2022-07-19 18:29:12 -0300

    carrousel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import './styles/App.css';
 import Main from './components/Main';
+import Carrousel from './components/Carrousel';
+import Footer from './components/Footer';
+import DetailsCarousel from './components/CarouselProductDetails';
 
 function App() {
   return (
     <div className="App">
       <h1>Ultimo esfuerzo!</h1>
       <Main></Main>
+      <Carrousel />
+      {/* <DetailsCarousel /> */}
+      <Footer />
     </div>
   );
 }
