5b3efa3e6ea92897a31af6c681bc13ad72b7401e
Marco Castellacci <marco.castellacci87@gmail.com>
2022-07-20 11:53:59 -0300

    create redux/actions

---
+++ frontend/src/redux/reducers/productReducers.js
@@ -0,0 +1,37 @@
+const initialState = {
+    products: [],
+    onePrduct: {},
+    filterPerCategory: [],
+    favoriteProducts: [],
+}
+
+const productReducers = (state = initialState, action) => {
+        switch (action.type) {
+            case 'GET_PRODUCTS':
+                return {
+                    ...state,
+                    products: action.payload,
+                    filterPerCategory: action.payload,
+                }
+            case 'GET_ONE_PRODUCT':
+                return {
+                    ...state,
+                    onePrduct: action.payload,
+                }
+            case 'FILTER_PER_CATEGORY':
+                let filter = state.products.filter( product => product.category === action.payload )
+                return {
+                    ...state,
+                    filterPerCategory: filter,  
+                }
+            case 'ADD_FAVORITE':
+                return {
+                    ...state,
+                    favoriteProducts: [...state.favoriteProducts, action.payload],
+                }
+            default:
+                return state
+    }
+}
+
+export default productReducers;
