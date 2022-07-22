755a86ccf926b8a8dfae1535d0a70746afc49dd7
Fiorela A Cristaldo <cristaldofiorela@gmail.com>
2022-07-22 20:57:02 -0300

    sign estilos modificados

--- src/components/SignIn-Out/SignInOut.jsx
+++ src/components/SignIn-Out/SignInOut.jsx
@@ -20,46 +20,46 @@ export default function SignInOut() {
             <div className="sign-in-out">
                 <div className="container" id="container">
                     <div className="form-container sign-up-container">
-                        <form action="#">
-                            <h1>Create Account</h1>
+                        <form className="formSign_F" action="#">
+                            <h1 className="titleSignUp_F">Create Account</h1>
                             <div className="social-container">
                                 <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
                                 <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
                                 <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
                             </div>
-                            <span>or use your email for registration</span>
-                            <input type="text" placeholder="Full Name" />
-                            <input type="email" placeholder="Email" />
-                            <input type="password" placeholder="Password" />
-                            <button>Sign Up</button>
+                            <span className="spanSign_F">or use your email for registration</span>
+                            <input className="inputSign_F" type="text" placeholder="Full Name" />
+                            <input className="inputSign_F" type="email" placeholder="Email" />
+                            <input className="inputSign_F" type="password" placeholder="Password" />
+                            <button className="btnSign_F">Sign Up</button>
                         </form>
                     </div>
                     <div className="form-container sign-in-container">
-                        <form action="#">
-                            <h1>Sign in</h1>
+                        <form className="formSign_F"  action="#">
+                            <h1 className="titleSignUp_F">Sign in</h1>
                             <div className="social-container">
                                 <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
                                 <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
                                 <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
                             </div>
-                            <span>or use your account</span>
-                            <input type="email" placeholder="Email" />
-                            <input type="password" placeholder="Password" />
+                            <span className="spanSign_F">or use your account</span>
+                            <input className="inputSign_F" type="email" placeholder="Email" />
+                            <input className="inputSign_F" type="password" placeholder="Password" />
                             <a>Forgot your password?</a>
-                            <button>Sign In</button>
+                            <button className="btnSign_F">Sign In</button>
                         </form>
                     </div>
                     <div className="overlay-container">
                         <div className="overlay">
                             <div className="overlay-panel overlay-left">
-                                <h1>Welcome Back!</h1>
-                                <p>To keep connected with us please login with your personal info</p>
+                                <h1 className="titleSignUp_F">Welcome Back!</h1>
+                                <p className="signText_F">To keep connected with us please login with your personal info</p>
                                 <button className="ghost" onClick={signInButton}>Sign In</button>
                             </div>
                             <div className="overlay-panel overlay-right">
-                                <h1>Hello, Friend!</h1>
-                                <p>Enter your personal details and start journey with us</p>
-                                <button className="ghost" onClick={signUpButton}>Sign Up</button>
+                                <h1 className="titleSignUp_F">Hello, Friend!</h1>
+                                <p className="signText_F">Enter your personal details and start journey with us</p>
+                                <button className="ghost " onClick={signUpButton}>Sign Up</button>
                             </div>
                         </div>
                     </div>
