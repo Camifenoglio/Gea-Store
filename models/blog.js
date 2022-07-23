1b67283a29ad23f730a92ac7e058e4461e586924
Marco Castellacci <marco.castellacci87@gmail.com>
2022-07-23 17:21:55 -0300

    correctioon code

--- models/blog.js
+++ models/blog.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema ({
-   
-    title: {type:String, required:true},
-    fileUpload: {type:String, required:true},
-    description: {type:String},
-    date: {type:Date},
-    likes: {type:Array},
+const blogSchema = new mongoose.Schema({
+
+    title: { type: String, required: true },
+    fileUpload: { type: String, required: true },
+    description: { type: String },
+    date: { type: Date },
+    likes: { type: Array },
     comments: [{
-        comment: {type: String},
-        userId: {type:mongoose.Types.ObjectId, ref:'User'},
+        comment: { type: String },
+        userId: { type: mongoose.Types.ObjectId, ref: 'User' },
     }]
 })
 
-const Blog = mongoose.model('blog',blogSchema)
-module.exports =Blog
+const Blog = mongoose.model('blog', blogSchema)
+module.exports = Blog
