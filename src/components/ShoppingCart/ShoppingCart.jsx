98d088cbe8e6c340d113da6ec0b1dfca8fa721c8
albertoespinozaf <espinozaf.alberto@gmail.com>
2022-07-27 10:23:42 -0400

    new commit

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -55,52 +55,60 @@ const ShoppingCart = () => {
     });
 
     return (
-        <Container maxWidth="md">
-            <h2>Shopping Cart</h2>
-            <h3>Products</h3>
-            <Stack direction="column" spacing={1} divider={<Divider orientation="horizontal" flexItem />}>
-                {cart.map((item) => {
-                    const product = productsById[item.productId];
-                    return (
-                        <Stack p={{ xs: 2, sm: 4 }} key={item.productId} direction="row" spacing={4} >
-                            <Stack justifyContent="center">
-                                <div className="image-container">
-                                    <img src={product.image} alt={product.name} />
-                                </div>
-                            </Stack>
-                            <Stack justifyContent="center">
-                                <h3>{product.name}</h3>
-                                <Box display={{ xs: 'none', md: 'block' }}>{product.description}</Box>
-                            </Stack>
-                            <Stack justifyContent="center" direction={{ xs: "column", sm: "row" }} spacing={2}>
-                                <Stack justifyContent="center" spacing={2}>
-                                    <Stack spacing={2} direction="row">
-                                        <button onClick={() => dispatch(addToCart(product._id))}>+</button>
-                                        <div>
-                                            {item.quantity}
-                                        </div>
-                                        <button onClick={() => dispatch(delFromCart(product._id))}>-</button>
-                                    </Stack>
-                                    <button onClick={() => dispatch(delFromCart(product._id, true))}>Remove Product
-                                    </button>
+        <Container className='main'>
+
+            <Stack>
+                <Stack direction="column" spacing={1} divider={<Divider orientation="horizontal" flexItem />}>
+                    <Stack textAlign='center'>
+                        <h2>Shopping Cart</h2>
+                        <h3>Products</h3>
+
+                    </Stack>
+                    {cart.map((item) => {
+                        const product = productsById[item.productId];
+                        return (
+                            <Stack p={{ xs: 2, sm: 4 }} key={item.productId} direction="row" spacing={4} sx={{ xs: 2, sm: 4 }}>
+                                <Stack justifyContent="center" >
+                                    <div className="image-container">
+                                        <img src={product.image} alt={product.name} />
+                                    </div>
                                 </Stack>
-                                <Stack justifyContent="center">{new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD' }).format(product.price)}</Stack>
+                                <Stack justifyContent="center">
+                                    <h3>{product.name}</h3>
+                                    <Box display={{ xs: 'none', md: 'block' }}>{product.description}</Box>
+                                </Stack>
+                                <Stack justifyContent="center" direction={{ xs: "column", sm: "row" }} spacing={2}>
+                                    <Stack justifyContent="center" spacing={2}>
+                                        <Stack spacing={2} direction="row">
+                                            <button onClick={() => dispatch(addToCart(product._id))}>+</button>
+                                            <div>
+                                                {item.quantity}
+                                            </div>
+                                            <button onClick={() => dispatch(delFromCart(product._id))}>-</button>
+                                        </Stack>
+                                        <button onClick={() => dispatch(delFromCart(product._id, true))}>Remove Product
+                                        </button>
+                                    </Stack>
+                                    <Stack justifyContent="center">{new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD' }).format(product.price)}</Stack>
+                                </Stack>
                             </Stack>
-                        </Stack>
-                    )
-                })}
+                        )
+                    })}
+                </Stack>
             </Stack>
+            <Stack>
 
-            <Stack direction="row" spacing={2} alignItems="center">
-                <h4>Total</h4>
-                <div>{new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD' }).format(total)}</div>
-                <button onClick={() => {
-                    handleDelete()
-                }
-                }>Clear Cart</button>
+                <Stack direction="row" spacing={2} alignItems="center">
+                    <h4>Total</h4>
+                    <div>{new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD' }).format(total)}</div>
+                    <button onClick={() => {
+                        handleDelete()
+                    }
+                    }>Clear Cart</button>
+                </Stack>
+                <Paypal />
+
             </Stack>
-            <Paypal />
-
 
 
 
